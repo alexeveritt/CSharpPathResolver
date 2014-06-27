7dc9b5faadd928b3f323bc14621d30bd90b166c8
alexeveritt <alex.everitt@bytechaser.com>
2014-06-27 20:58:40 +0100

    building up pathresolver

--- src/PathResolver.cs
+++ src/PathResolver.cs
@@ -1,10 +1,64 @@
 ﻿namespace PathResolver
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
     public class PathResolver
     {
         public string ResolvePath(string basePath, string relativePath)
         {
+            var relativePathParts = SplitRelativePath(relativePath);
+
+            var pathInfo = this.SplitUpBasePath(basePath);
+            var root = pathInfo.Item1;
+            var seperator = pathInfo.Item2;
+            var basePathParts = pathInfo.Item3;
+
+            for (var i = 0; i < relativePathParts.Count; i++)
+            {
+                var relativePathPart = relativePathParts[i];
+                if (relativePathPart == "..")
+                {
+                    basePathParts.Remove(basePathParts[basePathParts.Count - 1]);
+                }
+                else
+                {
+                    basePathParts.Add(relativePathPart);
+                }
+            }
+
+            var resolvedPath = root + string.Join(seperator, basePathParts);
+            return resolvedPath;
+        }
+
+        private static string GetRootFromPath(string path)
+        {
+            if (path.Contains(":\\"))
+            {
+            }
+            else if (path.StartsWith("\\\\"))
+            {
+            }
+
             return string.Empty;
+        }
+
+        private static List<string> SplitRelativePath(string relativePath)
+        {
+            var parts =
+                relativePath.Split(@"\/".ToCharArray()).Where(part => !string.IsNullOrWhiteSpace(part.Trim())).ToList();
+            return parts;
+        }
+
+        private Tuple<string, string, List<string>> SplitUpBasePath(string basePath)
+        {
+            var root = GetRootFromPath(basePath);
+            var pathSeperator = basePath.Contains("\\") ? "\\" : "/";
+            basePath = basePath.TrimStart(root.ToCharArray());
+            var pathSections = SplitRelativePath(basePath);
+
+            return new Tuple<string, string, List<string>>(root, pathSeperator, pathSections);
         }
     }
 }
