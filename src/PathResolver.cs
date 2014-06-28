9f5cbff014f549146d8f8c7377119911c3ff0b1e
alexeveritt <alex.everitt@bytechaser.com>
2014-06-28 14:44:03 +0100

    Refactored resolver and added some tests

--- src/PathResolver.cs
+++ src/PathResolver.cs
@@ -6,59 +6,126 @@
 
     public class PathResolver
     {
+        private readonly IDictionary<PathStyle, Func<string, string, string>> pathResolver;
+
+        public PathResolver()
+        {
+            pathResolver = new Dictionary<PathStyle, Func<string, string, string>>
+                               {
+                                   { PathStyle.Directory, ResolveDirectoryPath },
+                                   { PathStyle.Unc, ResolveUncPath },
+                                   { PathStyle.Http, ResolveHttpPath },
+                                   { PathStyle.Linux, ResolveLinuxPath }
+                               };
+        }
+
+        private enum PathStyle
+        {
+            Directory = 1,
+            Unc = 2,
+            Http = 3,
+            Linux = 4
+        }
+
+        public static string ResolveDirectoryPath(string basePath, string relativePath)
+        {
+            var pos = basePath.IndexOf(@":\", StringComparison.Ordinal) + 2;
+            var root = basePath.Substring(0, pos);
+            var path = basePath.Substring(pos);
+
+            var newPath = BuildNewPath(path, relativePath);
+            return string.Format("{0}{1}", root, string.Join(@"\", newPath));
+        }
+
+        public static string ResolveHttpPath(string basePath, string relativePath)
+        {
+            var posProtocolSep = basePath.IndexOf("//", StringComparison.Ordinal) + 2;
+            var pos = basePath.IndexOf("/", posProtocolSep, StringComparison.Ordinal);
+            var root = pos > 0 ? basePath.Substring(0, pos) : basePath;
+            var path = pos > 0 ? basePath.Substring(pos) : string.Empty;
+
+            var newPath = BuildNewPath(path, relativePath);
+            return newPath.Any() ? string.Format("{0}/{1}", root, string.Join("/", newPath)) : root;
+        }
+
+        public static string ResolveLinuxPath(string basePath, string relativePath)
+        {
+            if (!basePath.StartsWith("/"))
+            {
+                throw new Exception("Invalid Path format. Path must start with a single /");
+            }
+
+            const int Pos = 1;
+            var path = basePath.Length > 1 ? basePath.Substring(Pos) : string.Empty;
+
+            var newPath = BuildNewPath(path, relativePath);
+            return string.Format("/{0}", string.Join("/", newPath));
+        }
+
+        public static string ResolveUncPath(string basePath, string relativePath)
+        {
+            if (!basePath.StartsWith(@"\\"))
+            {
+                throw new Exception("Invalid Path format. Path must start with \\");
+            }
+
+            var pos = basePath.IndexOf(@"\", 2, StringComparison.Ordinal);
+            var root = pos > 0 ? basePath.Substring(0, pos) : basePath;
+            var path = pos > 0 ? basePath.Substring(pos) : string.Empty;
+
+            var newPath = BuildNewPath(path, relativePath);
+            return newPath.Any() ? string.Format(@"{0}\{1}", root, string.Join(@"\", newPath)) : root;
+        }
+
         public string ResolvePath(string basePath, string relativePath)
         {
-            var relativePathParts = SplitRelativePath(relativePath);
+            return pathResolver[GetPathStyle(basePath)](basePath, relativePath);
+        }
 
-            var pathInfo = this.SplitUpBasePath(basePath);
-            var root = pathInfo.Item1;
-            var seperator = pathInfo.Item2;
-            var basePathParts = pathInfo.Item3;
+        private static IList<string> BuildNewPath(string path, string relativePath)
+        {
+            var basePathSegments = SplitPath(path);
+            var relativePathSegments = SplitPath(relativePath);
 
-            for (var i = 0; i < relativePathParts.Count; i++)
+            foreach (var relativeSegment in relativePathSegments)
             {
-                var relativePathPart = relativePathParts[i];
-                if (relativePathPart == "..")
+                if (relativeSegment == "..")
                 {
-                    basePathParts.Remove(basePathParts[basePathParts.Count - 1]);
+                    basePathSegments.Remove(basePathSegments[basePathSegments.Count - 1]);
                 }
                 else
                 {
-                    basePathParts.Add(relativePathPart);
+                    basePathSegments.Add(relativeSegment);
                 }
             }
 
-            var resolvedPath = root + string.Join(seperator, basePathParts);
-            return resolvedPath;
+            return basePathSegments;
         }
 
-        private static string GetRootFromPath(string path)
+        private static PathStyle GetPathStyle(string basePath)
         {
-            if (path.Contains(":\\"))
+            var lookUp = new List<KeyValuePair<Func<string, bool>, PathStyle>>
+                             {
+                                 new KeyValuePair<Func<string, bool>, PathStyle>(x => x.Contains(@":\"), PathStyle.Directory),
+                                 new KeyValuePair<Func<string, bool>, PathStyle>(x => x.StartsWith(@"\\"), PathStyle.Unc),
+                                 new KeyValuePair<Func<string, bool>, PathStyle>(x => x.Contains("//"), PathStyle.Http),
+                                 new KeyValuePair<Func<string, bool>, PathStyle>(x => x.StartsWith("/"), PathStyle.Linux)
+                             };
+
+            var pathStyle = lookUp.Where(x => x.Key(basePath)).Select(x => x.Value).FirstOrDefault();
+            if (0 == pathStyle)
             {
-            }
-            else if (path.StartsWith("\\\\"))
-            {
+                throw new Exception("Path Style not supported");
             }
 
-            return string.Empty;
+            return pathStyle;
         }
 
-        private static List<string> SplitRelativePath(string relativePath)
+        private static List<string> SplitPath(string relativePath)
         {
             var parts =
                 relativePath.Split(@"\/".ToCharArray()).Where(part => !string.IsNullOrWhiteSpace(part.Trim())).ToList();
             return parts;
-        }
-
-        private Tuple<string, string, List<string>> SplitUpBasePath(string basePath)
-        {
-            var root = GetRootFromPath(basePath);
-            var pathSeperator = basePath.Contains("\\") ? "\\" : "/";
-            basePath = basePath.TrimStart(root.ToCharArray());
-            var pathSections = SplitRelativePath(basePath);
-
-            return new Tuple<string, string, List<string>>(root, pathSeperator, pathSections);
         }
     }
 }
--- src/PathResolverTest.cs
+++ src/PathResolverTest.cs
@@ -5,20 +5,86 @@
     [TestFixture]
     public class PathResolverTest
     {
-        [TestCase(@"C:\", @"path1", Result = @"C:\path1")]
-        [TestCase(@"c:\path1\path2", @"path3", Result = @"")]
-        [TestCase(@"c:\path1\path2", @"..", Result = @"..")]
-        [TestCase(@"c:\path1\path2", @"../path3", Result = @"")]
-        [TestCase(@"c:\path1\path2", @"../../", Result = @"")]
-        [TestCase(@"c:\path1\path2", @"../../path3", Result = @"")]
-        [TestCase(@"c:\path1\path2", @"../../..", Result = @"")]
-        [TestCase(@"\\root\", @"..", Result = @"error")]
-        [TestCase(@"//", @"", Result = @"")]
+        [TestCase(@"c:\", @"path1", Result = @"c:\path1")]
+        [TestCase(@"c:\path1\path2", @"path3", Result = @"c:\path1\path2\path3")]
+        [TestCase(@"c:\path1\path2", @"..", Result = @"c:\path1")]
+        [TestCase(@"c:\path1\path2", @"..\..\path3", Result = @"c:\path3")]
+        [TestCase(@"\\path1\path2", @"path3", Result = @"\\path1\path2\path3")]
+        [TestCase(@"\\path1\path2", @"..", Result = @"\\path1")]
+        [TestCase(@"\\path1\path2", @"..\path3", Result = @"\\path1\path3")]
+        [TestCase(@"\\path1\", @"path2", Result = @"\\path1\path2")]
+        [TestCase(@"//path1/path2", @"path3", Result = @"//path1/path2/path3")]
+        [TestCase(@"//path1/path2", @"../path3", Result = @"//path1/path3")]
+        [TestCase(@"//path1/path2", @"../path3/path4", Result = @"//path1/path3/path4")]
+        [TestCase(@"http://path1/path2", @"path3", Result = @"http://path1/path2/path3")]
+        [TestCase(@"http://path1/path2", @"../path3", Result = @"http://path1/path3")]
+        [TestCase(@"http://path1/path2", @"../path3/path4", Result = @"http://path1/path3/path4")]
+        [TestCase(@"http://path1:23512/path2", @"path3", Result = @"http://path1:23512/path2/path3")]
+        [TestCase(@"http://path1:23512/path2", @"../path3", Result = @"http://path1:23512/path3")]
+        [TestCase(@"http://path1:23512/path2", @"../path3/path4", Result = @"http://path1:23512/path3/path4")]
+        [TestCase(@"/", @"path1", Result = @"/path1")]
+        [TestCase(@"/path1/path2", @"path3", Result = @"/path1/path2/path3")]
+        [TestCase(@"/path1/path2", @"..", Result = @"/path1")]
+        [TestCase(@"/path1/path2", @"../..", Result = @"/")]
+        [TestCase(@"/path1/path2", @"../path3", Result = @"/path1/path3")]
+        [TestCase(@"/path1/path2", @"../../path3", Result = @"/path3")]
         public string ResolvePathTest(string basePath, string relativePath)
         {
             var resolver = new PathResolver();
             var resolvedPath = resolver.ResolvePath(basePath, relativePath);
             return resolvedPath;
+        }
+
+        public string ResolvingDirectoryPathShouldNotChangeCase()
+        {
+            return null;
+        }
+
+        [TestCase(@"c:\", @"path1", Result = @"c:\path1")]
+        [TestCase(@"c:\path1\path2", @"path3", Result = @"c:\path1\path2\path3")]
+        [TestCase(@"c:\path1\path2", @"..", Result = @"c:\path1")]
+        [TestCase(@"c:\path1\path2", @"..\..\path3", Result = @"c:\path3")]
+        public string ResolveDirectoryPath(string basePath, string relativePath)
+        {
+            var result = PathResolver.ResolveDirectoryPath(basePath, relativePath);
+            return result;
+        }
+
+        [TestCase(@"\\path1\path2", @"path3", Result = @"\\path1\path2\path3")]
+        [TestCase(@"\\path1\path2", @"..", Result = @"\\path1")]
+        [TestCase(@"\\path1\path2", @"..\path3", Result = @"\\path1\path3")]
+        [TestCase(@"\\path1\", @"path2", Result = @"\\path1\path2")]
+        public string ResolveUncPath(string basePath, string relativePath)
+        {
+            var result = PathResolver.ResolveUncPath(basePath, relativePath);
+            return result;
+        }
+
+        [TestCase(@"//path1/path2", @"path3", Result = @"//path1/path2/path3")]
+        [TestCase(@"//path1/path2", @"../path3", Result = @"//path1/path3")]
+        [TestCase(@"//path1/path2", @"../path3/path4", Result = @"//path1/path3/path4")]
+        [TestCase(@"http://path1/path2", @"path3", Result = @"http://path1/path2/path3")]
+        [TestCase(@"http://path1/path2", @"../path3", Result = @"http://path1/path3")]
+        [TestCase(@"http://path1/path2", @"../path3/path4", Result = @"http://path1/path3/path4")]
+        [TestCase(@"http://path1:23512/path2", @"path3", Result = @"http://path1:23512/path2/path3")]
+        [TestCase(@"http://path1:23512/path2", @"../path3", Result = @"http://path1:23512/path3")]
+        [TestCase(@"http://path1:23512/path2", @"../path3/path4", Result = @"http://path1:23512/path3/path4")]
+        public string ResolveHttpPath(string basePath, string relativePath)
+        {
+            var result = PathResolver.ResolveHttpPath(basePath, relativePath);
+            return result;
+        }
+
+        [TestCase(@"/", @"path1", Result = @"/path1")]
+        [TestCase(@"/path1/path2", @"path3", Result = @"/path1/path2/path3")]
+        [TestCase(@"/path1/path2", @"..", Result = @"/path1")]
+        [TestCase(@"/path1/path2", @"../..", Result = @"/")]
+        [TestCase(@"/path1/path2", @"../path3", Result = @"/path1/path3")]
+        [TestCase(@"/path1/path2", @"../../path3", Result = @"/path3")]
+        public string ResolveLinuxPath(string basePath, string relativePath)
+        {
+            var result = PathResolver.ResolveLinuxPath(basePath, relativePath);
+            return result;
         }
     }
 }
